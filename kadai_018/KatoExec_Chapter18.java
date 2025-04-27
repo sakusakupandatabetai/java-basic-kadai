aced51ec10e64c7e5c87eccde37f07040ee1a7ea
sakusakupandatabetai <sakurayokoo49@gmail.com>
2025-04-27 20:00:56 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+    public static void main(String[] args) {
+        Kato_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+        Kato_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+        Kato_Chapter18 KatoHanako = new KatoHanako_Chapter18();
+
+        KatoTaro.execIntroduce();
+        KatoIchiro.execIntroduce();
+        KatoHanako.execIntroduce();
+    }
+}
