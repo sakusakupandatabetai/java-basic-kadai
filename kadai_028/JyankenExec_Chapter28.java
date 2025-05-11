dd7d38a11e21f7a403766d330c24d02c12b80a4d
sakusakupandatabetai <sakurayokoo49@gmail.com>
2025-05-11 16:46:42 +0900

    Add files via upload

---
+++ kadai_028/JyankenExec_Chapter28.java
@@ -0,0 +1,13 @@
+package kadai_028;
+
+public class JyankenExec_Chapter28 {
+ public static void main(String[] args) {
+	 //インスタンス化
+	 Jyanken_Chapter28 jyanken = new Jyanken_Chapter28();
+	//じゃんけんのゲームを実行
+			jyanken.playGame();
+	 
+	 
+	 
+ }
+}
